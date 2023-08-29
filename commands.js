ad59c9b4fa08a34d3cf63b67dbbd2fcb04ebc8de
Ajay H <hajay180505@gmail.com>
2023-08-29 21:35:07 +0530

    First commit

---
+++ commands.js
@@ -0,0 +1,104 @@
+
+var password = "fearless";
+var linkedin = "https://www.linkedin.com/in/hajay180505/";
+var instagram = "https://www.instagram.com/hajay180505/";
+var github = "https://github.com/hajay180505";
+var email = 'mailto:hajay180505@gmail.com';
+
+whois = [
+  "<br>",
+  "¡Hola! 👋 ¿Cómo estás?",
+  "Wait... don't go yet.. Imma continue in english..",
+  'I am Ajay H (<a href="https://www.youtube.com/watch?v=rFjJs6ZjPe8" target="_blank">Mr.Perfectly fine</a>), an undergraduate student pursuing Masters in Software Systems at <a href="https://www.psgtech.edu/" target="_blank"> PSG Tech </a>. ',
+  "I like origami, papercraft and recently, embroidering.",
+  "I am interested in open source softwares , linux (not arch rn) and back-end development,",
+  'as you are seeing with the interface that you are presented with. :)',
+  'I am a selenator turned swiftie and a vim enthusiast.',
+  'Feel free to contact me on a sane time!',
+  'Now, normally there will be a nice conclusion, but I am infamous for my awkward conclusions.',
+  'So, I bid thy farewell.. have a great day!'  ,
+  "<br>"
+];
+
+whatami = [
+  "<br>",
+  "Mostly a cat, sometimes a human",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/hajay180505' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/hajay180505' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/hajay180505' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  'Still learning.. meanwhile, goto my github profile <a href="'+ github + '" target="_blank">here' + "</a>",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Ajay?',
+  '<span class="command">whatami</span>        What am I?',
+  '<span class="command">social</span>         Stalk me',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           It doesn\'t help',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">flex</span>           I don\'t use Arch BTW',
+  '<span class="command">display me</span>     The actor BTS',
+  "<br>",
+];
+// todo: flex
+flex = [
+  '--------------------------------------------',
+  '| <span class="index">Ajay H : An interesting person, probably</span> |',
+  '--------------------------------------------',
+  '<br>',
+  '&nbsp;█████╗      ██╗ █████╗ ██╗   ██╗    ██╗  ██╗',
+  '██╔══██╗     ██║██╔══██╗╚██╗ ██╔╝    ██║  ██║',
+  '███████║     ██║███████║ ╚████╔╝     ███████║',
+  '██╔══██║██   ██║██╔══██║  ╚██╔╝      ██╔══██║',
+  '██║  ██║╚█████╔╝██║  ██║   ██║       ██║  ██║',
+  '╚═╝  ╚═╝ ╚════╝ ╚═╝  ╚═╝   ╚═╝       ╚═╝  ╚═╝',
+  '<br>',
+  '<span class="color2">Welcome to my interactive web terminal!</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
+
+/*
+'          _               _    _ ',
+'    /\   (_)             | |  | |',
+'   /  \   _  __ _ _   _  | |__| |',
+'  / /\ \ | |/ _` | | | | |  __  |',
+' / ____ \| | (_| | |_| | | |  | |',
+'/_/    \_\ |\__,_|\__, | |_|  |_|',
+'        _/ |       __/ |         ',
+'       |__/       |___/          ', */
+
+
+
+// ' █████╗      ██╗ █████╗ ██╗   ██╗    ██╗  ██╗',
+// '██╔══██╗     ██║██╔══██╗╚██╗ ██╔╝    ██║  ██║',
+// '███████║     ██║███████║ ╚████╔╝     ███████║',
+// '██╔══██║██   ██║██╔══██║  ╚██╔╝      ██╔══██║',
+// '██║  ██║╚█████╔╝██║  ██║   ██║       ██║  ██║',
+// '╚═╝  ╚═╝ ╚════╝ ╚═╝  ╚═╝   ╚═╝       ╚═╝  ╚═╝',
+                                             
+pic = [
+    '<br>',
+    '<img src="pfp.jpeg" width="120" height="160"/>',
+    '<br>'
+]
